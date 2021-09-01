4a977792f1aa5d58e0b07ade645b45139402871b
NANI <Yan74-47@mail.ru>
2021-09-01 21:34:33 +0500

    Произведите ретинизацию и pp

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,8 @@
 const btnToggle = document.querySelector(".page-header__toggle");
 const mainNav = document.querySelector(".main-nav");
+const btnBack = document.querySelector(".slider__toggle--back");
+const btnNext = document.querySelector(".slider__toggle--next");
+const sliderMobile = document.querySelector(".slider__mobile");
 
 mainNav.classList.add("main-nav--closed");
 
@@ -7,3 +10,11 @@ btnToggle.addEventListener("click", function (evt) {
   btnToggle.classList.toggle("page-header__toggle--close");
   mainNav.classList.toggle("main-nav--closed");
 });
+
+btnNext.addEventListener("click", function (evt) {
+  sliderMobile.classList.add("slider__mobile--scale");
+});
+btnBack.addEventListener("click", function (evt) {
+  sliderMobile.classList.remove("slider__mobile--scale");
+});
+
