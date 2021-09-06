a0acc683037d0dc7e29562c750099749facbafa0
NANI <Yan74-47@mail.ru>
2021-09-06 10:41:35 +0500

    Выполняет автоматизацию проекта

--- gulpfile.js
+++ gulpfile.js
@@ -144,7 +144,6 @@ const reload = (done) => {
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
-  gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
 // Build
